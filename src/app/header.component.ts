2a6b702a74e58c99bea1e74dde8478b12ee5257d
Hinojosa Hernandez <roberto.hinojosahernandez@one.verizon.com>
2017-02-21 14:10:50 -0600

    created header component files only

---
+++ src/app/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import * as _ from 'lodash';
+import { ModelService } from './model.service';
+@Component({
+    selector: 'bob-header',
+    styleUrls: ['header.component.scss'],
+    templateUrl: 'header.component.html',
+})
+export class HeaderComponent {
+    constructor(
+        modelService: ModelService,
+    ) {
+    }
+}
